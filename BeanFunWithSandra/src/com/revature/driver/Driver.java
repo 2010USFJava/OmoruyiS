bb4219e34402d1e51934ae8e7525a8a134843598
Sandra <somoruyi@icloud.com>
2020-10-20 16:02:22 -0500

    BeanFunwithSandra

---
+++ BeanFunWithSandra/src/com/revature/driver/Driver.java
@@ -0,0 +1,13 @@
+package com.revature.driver;
+import com.revature.person.Person;
+
+
+public class Driver {
+	public static void main(String[] args) {
+		Person a = new Person("Mike", "blue", 99);
+		Person b = new Person("Mike", "red", 80);
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
