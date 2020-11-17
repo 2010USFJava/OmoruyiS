9374304db4b826ed17a1cd725681c6396767dcfa
Sandra <somoruyi@icloud.com>
2020-11-16 23:47:35 -0600

    updated readme

--- JDBCBank.sql
+++ JDBCBank.sql
@@ -1,45 +1,95 @@
 /*
  * JDBC Bank Database Creation Script
- * Schemas : public 
- * Tables  : customers
- * Columns :
- *	1) pid (int)
- *	2) pname (varchar(30))
+ * Schemas : jdbcbank 
+ * Tables  : register, account, & customer
+ *
  */
-
---	private int uid;
---	private String username; 
---	private String password;
---	private String first;
---	private String last;
---	private double balance;
---	private int qty;
---	private long accountNumber;
---	private int typeOfAccount;
+--create sequence mySeq;
 
 
+create schema if not exists jdbcbank authorization root; --creates scheme jdbcbank
+set search_path to jdbcbank;
 
-create table customers(
-uid integer primary key, --Sets primary key
+
+create table register( --customer object table
+uid serial primary key, --Sets primary key
 username varchar(30),
-pass varchar(30),
-firstname varchar(30),
-lastname varchar(30)
+password varchar(30),
+first varchar(30),
+last varchar(30)
 );
 
-insert into customers values (1,'lebron', 'password', 'lebron', 'james'); --(DML) insert
+create table account( --account object table
+aid serial primary key, --Sets primary keyint qty,
+balance double precision,
+qty int,
+deposit double precision,
+withdrawal double precision,
+username varchar(30)
+);
+ 
+create table customer( --which customer has which account
+uid serial,
+aid serial,
+foreign key (uid) references register(uid) on delete cascade,
+foreign key (aid) references account(aid) on delete cascade
+);
 
-create table account(
-uid integer primary key, --Sets primary key
-balance integer,
-bankacctid integer
+create table assets(
+bid serial,
+bank varchar(30),
+equity double precision
 );
 
 
-insert into account values (1,25,001);
+--DML select key
+select * from register; --view register table
+select * from account;  --view account table
+select * from customer; --view customers table
+select * from assets; --view aggregation table
 
-select * from customers; --(DML) select
-select * from account;
+--drop table assets;
+--drop table customer;
+--drop table register;
+--drop table account;
+
+insert into assets (bid) values(1);
+
+update assets
+set bank='casino_royal'
+where bid=1;
+
+
+--select sum(balance) from account) 
+
+/*
+ * TEST & DEBUGGING COMMANDS
+ */ 
+
+--user defined function
+--select count(*) as uid
+--from register;
+
+--update account
+--set deposit=00.00
+--where aid=10003;
+
+--update account
+--set qty=1
+--where aid=10002;
+
+--update register 
+--set first='kevin',
+--last='durant',
+--password='password' 
+--where uid=2;
+
+--delete from register 
+--where uid =3;
+
+
+
+
 
 
 
---
+++ OmoruyiJDBCBank/README.md
@@ -0,0 +1,33 @@
+# README 
+# JDBC Bank Application 
+
+
+[1] Run JDBC Bank Application
+
+[1.1] In terminal run:
+
+      # git clone -b Project_0 https://github.com/2010USFJava/OmoruyiS.git
+      # cd  OmoruyiS/OmoruyiJDBCBank/
+      # cd src/main/java/com/revature
+
+[2] Compile Java Program 
+
+      # javac */*.java
+
+[3] Run program
+
+      # java driver/driver
+      
+[4] Main Screen View
+
+Welcome to Casino Royal Banking
+_________________________________
+
+	MAIN MENU
+_________________________________
+
+	[c]ustomer
+	[a]dmin
+
+	[q]uit
+_________________________________
